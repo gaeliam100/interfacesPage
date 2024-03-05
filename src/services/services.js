6eb7ef2af035c28c0d8a38896a13b91f7bf2f4f5
gaelivan740 <65014532+gaeliam100@users.noreply.github.com>
2024-03-05 09:43:39 -0600

    WebServerTunnels

---
+++ src/services/services.js
@@ -0,0 +1,15 @@
+export const getUserData = async () => {
+    const response = await fetch('https://kzx55n6w-3000.usw3.devtunnels.ms//users');
+    const data = await response.json();
+    return data;
+}
+
+export const getSingleUserData = async ({id}) => {
+    const response = await fetch(`https://kzx55n6w-3000.usw3.devtunnels.ms//users/${id}`);
+    const data = await response.json();
+
+    const response2 = await fetch(`https://kzx55n6w-3000.usw3.devtunnels.ms//trabajos/${id}`);
+    const data2 = await response2.json();
+
+    return { userData: data, trabajoData: data2 };
+}
